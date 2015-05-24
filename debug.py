a7c8d9dc04e9948a79eb7d9ceffcf13750f865c9
Christopher Su <christophersu9@gmail.com>
2015-05-23 23:17:49 -0700

    ignore

--- debug.py
+++ debug.py
@@ -7,4 +7,5 @@ import logstf_sync
 # logstf_sync.set_last_match_saved('last.txt', 100)
 # logstf_sync.save_to_disk.save_to_file('results', 'test.txt', 'hello world')
 # logstf_sync.run('last')
-logstf_sync.save_all_matches('last')
+# logstf_sync.save_all_matches('last')
+logstf_sync.parallel_save(831330, 833330)
